7adea6a8c3e4cdde8f523dd83a57f223dc326ec8
kettikat09 <katya.kryvosheeva.04@gmail.com>
2023-04-21 11:37:40 +0300

    1zavd
    1zavdannia

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Завдання 1. Дано список List <T> з об'єктами одного типу Obj, у якого в свою чергу є властивість Id.
+//Організувати пошук для знаходження елемента колекції по його властивості
+
+using System.Text.Json;
+public class Program
+{
+    public static void Main()
+    {
+        List<Person> person = new List<Person>();
+        for (int i = 0; i < 5; i++)
+            person.Add(new Person());
+        foreach (var item in person)
+        {
+            Console.WriteLine(item.Id);
+        }
+        Console.Write($"insert Id: ");
+        int SearchID = int.Parse(Console.ReadLine());
+        Person p = person.Find(x => x.Id == SearchID);
+        int idindex = person.IndexOf(p);
+        if (idindex != -1)
+            Console.WriteLine(idindex);
+        else
+            Console.WriteLine("ID not found");
+    }
+}
+public class Person
+{
+    public int Id;
+    public Person() { Random random = new Random(); Id = random.Next(1000, 10000); }
+}
