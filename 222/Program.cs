2d02a2baeb458417af1a40b68d5358948a62a356
kettikat09 <katya.kryvosheeva.04@gmail.com>
2023-04-21 11:44:39 +0300

    1laba
    1laba

---
+++ 222/Program.cs
@@ -0,0 +1,45 @@
+﻿//Завдання 2. Видалити зі словника елементи з однаковими значеннями. Записати результат у JSON файл.
+
+using Newtonsoft.Json;
+using System;
+using System.Text.Json;
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<int, int> elements = new Dictionary<int, int>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            elements.Add(i, random.Next(0, 5));
+            Console.Write(elements[i]+"\t");
+        }
+        Console.WriteLine("-----------"); 
+        List<int> list = new List<int>();
+        List<int> listkey = new List<int>();
+        foreach (int i in elements.Keys)
+        {
+            if (!list.Contains(elements[i]))
+            {
+                list.Add(elements[i]);
+                listkey.Add(i);
+            }
+        }
+        elements.Clear();
+        for (int i = 0; i<list.Count; i++)
+        {
+            elements.Add(listkey[i], list[i]);
+        }
+        foreach (var i in elements)
+        {
+            Console.WriteLine(i.Key + " " + i.Value);
+        }
+        string JSon = JsonConvert.SerializeObject(elements);
+
+        Console.WriteLine(JSon);
+
+
+    }
+
+
+}
---
+++ 333/Program.cs
@@ -0,0 +1,29 @@
+﻿//Завдання 3. Дано список: {1,2,3,1,4,5,2,2,1} Потрібно замінити повторювані числа (якщо число повторюється більше ніж 1 раз) на 0 всередині масиву (1) (3)
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+public class Program
+{
+    public static void Main()
+    {
+        List<int> list = new List<int> { 1, 2, 3, 1, 4, 5, 2, 2, 1 };
+
+        List<int> end = list.Take(3).ToList();
+        end.Add(0);
+        List<int> end2 = list.Skip(4).Take(2).ToList();
+        end2.Add(0);
+        end2.Add(0);
+        end2.Add(0);
+
+        list = Enumerable.Concat(end, end2).ToList();
+        foreach (int item in list)
+        {
+            Console.Write(item + " ");
+        }
+
+    }
+
+}
+
